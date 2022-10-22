0d8d6f940668e52bcee4d48b558840c898d9f1b3
paani9 <tpaani9@gmail.com>
2022-10-22 23:52:18 +0530

    commit8

---
+++ src/pac1/Pm1.java
@@ -0,0 +1,12 @@
+package pac1;
+
+public class Pm1 {
+	
+public static void main(String[] args) 
+
+{
+	
+
+System.out.println("GOD");
+}
+}
---
+++ src/practice/Practice1.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String i="1";
+ String j = ""+1;
+if(i==j) {
+	System.out.println("p");
+}
+	}
+
+}
